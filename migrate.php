65ebbc8b3ca698e7b0ec7bf6750482baa9b801ef
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2025-09-06 22:43:25 +0530

    new update

--- migrate.php
+++ migrate.php
@@ -3,30 +3,13 @@
 include_once("includes/db.php");
 
 // Create the 'drivers' table
-$sql = "CREATE TABLE `drivers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `driver_id` varchar(20) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `phone` varchar(15) NOT NULL,
-  `vehicle_number` varchar(20) NOT NULL,
-  `route` varchar(255) NOT NULL,
-  `driver_image` varchar(255) DEFAULT 'default_driver_dp.jpg',
-  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `driver_id` (`driver_id`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `phone` (`phone`),
-  UNIQUE KEY `vehicle_number` (`vehicle_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+$sql = "ALTER TABLE `students` ADD `driver_id` INT NULL DEFAULT NULL AFTER `car_route`";
+$sql2 = "ALTER TABLE `students` ADD `religion` VARCHAR(30) NULL DEFAULT NULL AFTER `email`, ADD `registration_no` VARCHAR(50) NULL DEFAULT NULL AFTER `religion`";
 
 // Execute the query
-if ($pdo->query($sql) === TRUE) {
-    echo "Table 'drivers' created successfully.";
+if ($pdo->query($sql) === TRUE && $pdo->query($sql2) === TRUE) {
+    echo "Columns created successfully.";
 } else {
     echo "Error creating table";
 }
-
-// Close the connection
 ?>
