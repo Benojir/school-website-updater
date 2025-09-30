cb9ba22d5feab1726a00d18a8acaf30b58b398e8
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2025-09-30 12:49:21 +0530

    dsfsd

--- migrate.php
+++ migrate.php
@@ -6,10 +6,10 @@ echo "<h3>Starting Database Migration...</h3><br>";
 
 // Array of .sql files to be executed in order
 $sql_files = [
-    'admission_full_paid_fees.sql',
-    'admission_unpaid_fees.sql',
-    'admission_fees_payment_history.sql',
-    'admission_partial_fees_payments.sql'
+    './admission_full_paid_fees.sql',
+    './admission_unpaid_fees.sql',
+    './admission_fees_payment_history.sql',
+    './admission_partial_fees_payments.sql'
 ];
 
 echo "<h4>Processing SQL files...</h4><br>";
