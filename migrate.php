0636f4b74a192c31cff1175af7b16e79e5acbe4d
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2025-10-08 22:41:57 +0530

    update

--- migrate.php
+++ migrate.php
@@ -1,10 +1,12 @@
 <?php
 
-include_once("includes/db.php");
+//include_once("includes/db.php"); No need to include this as the update.php already included
 
 echo "<h3>Starting Database Migration...</h3><br>";
 
-$query = "DROP TABLE old_admission_fees_table_ta_jetar_name_vule_gechhi";
+$query1 = "DROP TABLE student_admission_fees";
+$query2 = "ALTER TABLE admission_enquiries ADD preferred_by VARCHAR(100) NULL DEFAULT NULL AFTER class_id";
+$query3 = "ALTER TABLE admission_enquiries ADD religion VARCHAR(30) NULL DEFAULT NULL AFTER preffered_by";
     
-$pdo->exec($query);
+$pdo->exec($query1);
 ?>
