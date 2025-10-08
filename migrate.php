24da17c0376d4c4579ab93866a49aa82f4b39799
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2025-10-08 23:06:49 +0530

    update

--- migrate.php
+++ migrate.php
@@ -6,7 +6,7 @@ echo "<h3>Starting Database Migration...</h3><br>";
 
 $query1 = "DROP TABLE student_admission_fees";
 $query2 = "ALTER TABLE admission_enquiries ADD preferred_by VARCHAR(100) NULL DEFAULT NULL AFTER class_id";
-$query3 = "ALTER TABLE admission_enquiries ADD religion VARCHAR(30) NULL DEFAULT NULL AFTER preffered_by";
+$query3 = "ALTER TABLE admission_enquiries ADD religion VARCHAR(30) NULL DEFAULT NULL AFTER blood_group";
     
 $pdo->exec($query1);
 $pdo->exec($query2);
