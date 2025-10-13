64d4799c1600cf628223fb63e1117dfe9193770b
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2025-10-13 10:16:34 +0530

    update

--- migrate.php
+++ migrate.php
@@ -2,26 +2,18 @@
 
 //include_once("includes/db.php"); No need to include this as the update.php already included
 
-// echo "<h3>Starting Database Migration...</h3><br>";
+echo "<h3>Starting Database Migration...</h3><br>";
 
-// $query1 = "DROP TABLE student_admission_fees";
+$query1 = "ALTER TABLE `students` ADD `academic_year` VARCHAR(20) NULL DEFAULT NULL AFTER `promoted_date`";
 // $query2 = "ALTER TABLE admission_enquiries ADD preferred_by VARCHAR(100) NULL DEFAULT NULL AFTER class_id";
 // $query3 = "ALTER TABLE admission_enquiries ADD religion VARCHAR(30) NULL DEFAULT NULL AFTER blood_group";
     
-// $pdo->exec($query1);
+if ($pdo->exec($query1)) {
+	echo "Database migration successfully done!<br>";
+} else {
+	echo "Database migration failed!<br>";
+}
 // $pdo->exec($query2);
 // $pdo->exec($query3);
-
-
-echo "Deleting old receipt download file<br>";
-
-$file_path = "../parent/ajax/download-paid-receipt.php";
-
-if (file_exists($file_path)) {
-    unlink($file_path);
-    echo "Deleted old receipt download file successfully<br>";
-} else {
-    echo "Old receipt download file does not exist<br>";
-}
 
 ?>
