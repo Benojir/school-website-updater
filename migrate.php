67c82fcf92a065488b387995d5641b2e0ad9e4d0
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2025-10-13 10:21:11 +0530

    update

--- migrate.php
+++ migrate.php
@@ -4,16 +4,12 @@
 
 echo "<h3>Starting Database Migration...</h3><br>";
 
-$query1 = "ALTER TABLE `students` ADD `academic_year` VARCHAR(20) NULL DEFAULT NULL AFTER `promoted_date`";
-// $query2 = "ALTER TABLE admission_enquiries ADD preferred_by VARCHAR(100) NULL DEFAULT NULL AFTER class_id";
-// $query3 = "ALTER TABLE admission_enquiries ADD religion VARCHAR(30) NULL DEFAULT NULL AFTER blood_group";
-    
-if ($pdo->exec($query1)) {
-	echo "Database migration successfully done!<br>";
-} else {
-	echo "Database migration failed!<br>";
+try {
+    $query1 = "ALTER TABLE `students` ADD `academic_year` VARCHAR(20) NULL DEFAULT NULL AFTER `promoted_date`";
+    $pdo->exec($query1);
+    echo "Database migration successfully done!<br>";
+} catch (PDOException $e) {
+    echo "Database migration failed! Error: " . $e->getMessage() . "<br>";
 }
-// $pdo->exec($query2);
-// $pdo->exec($query3);
 
 ?>
