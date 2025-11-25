2fcb7fef25f277b100d7f9c9422dda47f8a1b34d
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2025-11-25 11:40:30 +0530

    updated

--- migrate.php
+++ migrate.php
@@ -5,10 +5,10 @@
 echo "Starting Database Migration...<br>";
 
 try {
-    $pdo->exec("ALTER TABLE `website_config` ADD `student_id_prefix` VARCHAR(10) NULL DEFAULT 'STU' AFTER `country_code`");
+    $pdo->exec("ALTER TABLE users
+  DROP remember_token,
+  DROP token_expiry");
 	
-	$pdo->exec("ALTER TABLE `school_information` ADD `meta_description` TEXT NULL DEFAULT NULL AFTER `description`, ADD `meta_keywords` TEXT NULL DEFAULT NULL AFTER `meta_description`");
-
     echo "Database migration successfully done!<br>";
 } catch (PDOException $e) {
     echo "Database migration failed! Error: " . $e->getMessage() . "<br>";
