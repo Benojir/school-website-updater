8dfe6bebbdd43d1b9028e651816c0a5e884c9298
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2025-12-22 21:48:17 +0530

    updated

--- migrate.php
+++ migrate.php
@@ -6,11 +6,12 @@ echo "Starting Migration Script...<br>";
 
 try {
   
-  $pdo->exec("ALTER TABLE `results` ADD `total_minor_marks` DECIMAL NULL DEFAULT '0' AFTER `remarks`, ADD `obtained_minor_marks` DECIMAL NULL DEFAULT '0' AFTER `total_minor_marks`, ADD `percentage_without_minor` DECIMAL NULL DEFAULT '0' AFTER `obtained_minor_marks`, ADD `grade_without_minor` VARCHAR(5) NULL DEFAULT NULL AFTER `percentage_without_minor`, ADD `remarks_without_minor` VARCHAR(100) NULL DEFAULT NULL AFTER `grade_without_minor`");
+  //$pdo->exec("ALTER TABLE `results` ADD `section_id` INT NULL DEFAULT NULL AFTER `class_id`");
   
-  $pdo->exec("ALTER TABLE `results` ADD `section_id` INT NULL DEFAULT NULL AFTER `class_id`");
-  
-  $pdo->exec("ALTER TABLE `settings_marksheet` ADD `section_based_ranking` TINYINT NOT NULL DEFAULT '0' AFTER `show_text_watermark`");
+  $pdo->exec("ALTER TABLE results
+MODIFY COLUMN total_minor_marks DECIMAL(10, 2) DEFAULT 0,
+MODIFY COLUMN obtained_minor_marks DECIMAL(10, 2) DEFAULT 0,
+MODIFY COLUMN percentage_without_minor DECIMAL(10, 2) DEFAULT 0");
 	
   echo "Database migration successfully done!<br>";
 
