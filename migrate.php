73a671b337de023956a59227776aafb37f984cb5
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2025-12-22 18:19:46 +0530

    updated

--- migrate.php
+++ migrate.php
@@ -6,23 +6,14 @@ echo "Starting Migration Script...<br>";
 
 try {
   
-  //$pdo->exec("ALTER TABLE admin_auth_sessions CHANGE device_name device_name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL");
+  $pdo->exec("ALTER TABLE `results` ADD `total_minor_marks` DECIMAL NULL DEFAULT '0' AFTER `remarks`, ADD `obtained_minor_marks` DECIMAL NULL DEFAULT '0' AFTER `total_minor_marks`, ADD `percentage_without_minor` DECIMAL NULL DEFAULT '0' AFTER `obtained_minor_marks`, ADD `grade_without_minor` VARCHAR(5) NULL DEFAULT NULL AFTER `percentage_without_minor`, ADD `remarks_without_minor` VARCHAR(100) NULL DEFAULT NULL AFTER `grade_without_minor`");
   
-  $pdo->exec("ALTER TABLE `settings_marksheet` ADD `show_text_watermark` TINYINT NULL DEFAULT NULL AFTER `include_minor_subjects_marks`");
+  $pdo->exec("ALTER TABLE `results` ADD `section_id` INT NULL DEFAULT NULL AFTER `class_id`");
   
-  $pdo->exec("ALTER TABLE `subjects` ADD `marksheet_order_by` INT NULL DEFAULT NULL AFTER `class_id`");
+  $pdo->exec("ALTER TABLE `settings_marksheet` ADD `section_based_ranking` TINYINT NOT NULL DEFAULT '0' AFTER `show_text_watermark`");
 	
-    echo "Database migration successfully done!<br>";
-	
-	//$delete_file_path = "../management/settings/school-information.php";
-	
-	//if(file_exists($delete_file_path)){
-		//if (!unlink($delete_file_path)) {
-			//echo "File delete failed!<br>";
-		//} else {
-			//echo "File delete success!<br>File path $delete_file_path <br>";
-		//}
-	//}
+  echo "Database migration successfully done!<br>";
+
 } catch (PDOException $e) {
     echo "Database migration failed! Error: " . $e->getMessage() . "<br>";
 } catch (Exception $e) {
