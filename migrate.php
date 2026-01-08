8e45bdc5c3f6a2543627a7ec0b64ef5287803f0a
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2026-01-08 19:37:18 +0530

    ufsdgfd

--- migrate.php
+++ migrate.php
@@ -8,9 +8,19 @@ try {
   
   //$pdo->exec("ALTER TABLE `results` ADD `section_id` INT NULL DEFAULT NULL AFTER `class_id`");
   
-  $pdo->exec("DROP TABLE IF EXISTS class_roll_numbers, graduation_logs, promotion_logs");
+  //$pdo->exec("DROP TABLE IF EXISTS class_roll_numbers, graduation_logs, promotion_logs");
 	
-  echo "Database migration successfully done!<br>";
+  //echo "Database migration successfully done!<br>";
+  $file_path = "../api/admin/put/student/export-students-data.php";
+  if (file_exists($file_path)){
+	  if (unlink($file_path)) {
+		  echo "File deleted";
+	  } else {
+		  echo "File not deleted";
+	  }
+  } else {
+	  echo "File not available";
+  }
 
 } catch (PDOException $e) {
     echo "Database migration failed! Error: " . $e->getMessage() . "<br>";
