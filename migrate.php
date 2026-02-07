322b712f2d0b02daa5e1ef7b48de928ff25c0078
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2026-02-07 14:55:26 +0530

    updated

--- migrate.php
+++ migrate.php
@@ -5,10 +5,17 @@
 echo "Starting Migration Script...<br>";
 
 try {
-	$sql1 = "ALTER TABLE `subjects` ADD `exclude_from_marksheet` TINYINT(1) NOT NULL DEFAULT '0' AFTER `class_id`";
-	$pdo->exec($sql1);
+	$sql1 = "ALTER TABLE `website_config` ADD `show_teachers_on_front_page` TINYINT(1) NOT NULL DEFAULT '0' AFTER `allow_online_payment`";
+	$sql2 = "ALTER TABLE `drivers` DROP `driver_id`";
+	$sql3 = "ALTER TABLE `teachers` DROP `teacher_id`";
+	$sql4 = "ALTER TABLE `drivers` ADD `aadhaar_number` VARCHAR(100) NULL DEFAULT NULL AFTER `route_id`";
 	
-	echo "Database migration successfully done!<br>";
+	$pdo->exec($sql1);
+	$pdo->exec($sql2);
+	$pdo->exec($sql3);
+	$pdo->exec($sql4);
+	
+	echo "Database migration successfully done! 😅<br>";
 
 } catch (PDOException $e) {
     echo "Database migration failed! Error: " . $e->getMessage() . "<br>";
