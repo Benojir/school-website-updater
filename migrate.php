801bf458a4bca9695466d36c114ab87f944e2fc0
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2026-02-28 13:44:23 +0530

    updated

--- migrate.php
+++ migrate.php
@@ -5,9 +5,15 @@
 echo "Starting Migration Script...<br>";
 
 try {
-	$sql1 = "ALTER TABLE `website_config` ADD `additional_settings` TEXT NULL DEFAULT NULL AFTER `currency_symbol`";
+	$sql1 = "ALTER TABLE `student_payment_history` ADD `entry_by` INT NULL DEFAULT NULL AFTER `method`";
+	$sql2 = "ALTER TABLE `users` ADD `security_pin` INT NOT NULL DEFAULT '123456' AFTER `email`";
+	$sql3 = "ALTER TABLE `admin_auth_sessions` ADD `security_pin` INT NULL DEFAULT NULL AFTER `full_name`";
+	$sql4 = "DELETE FROM admin_auth_sessions";
 	
 	$pdo->exec($sql1);
+	$pdo->exec($sql2);
+	$pdo->exec($sql3);
+	$pdo->exec($sql4);
 	
 	echo "Database migration successfully done! 😅<br>";
 
