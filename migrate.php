3d6ff98b5ab0c8e6d6a90e05ca6308183e9a9479
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2026-03-07 07:16:50 +0530

    updated

--- migrate.php
+++ migrate.php
@@ -5,7 +5,7 @@
 echo "Starting Migration Script...<br>";
 
 try {
-	$sql1 = "ALTER TABLE `teachers` ADD `position_in_school` VARCHAR(100) NULL DEFAULT NULL AFTER `phone`, ADD `village` VARCHAR(100) NULL DEFAULT NULL AFTER `position_in_school`, ADD `post_office` VARCHAR(100) NULL DEFAULT NULL AFTER `village`, ADD `police_station` VARCHAR(100) NULL DEFAULT NULL AFTER `post_office`, ADD `district` VARCHAR(100) NULL DEFAULT NULL AFTER `police_station`, ADD `pincode` VARCHAR(100) NULL DEFAULT NULL AFTER `district`";
+	$sql1 = "ALTER TABLE `teacher_applications` ADD `village` VARCHAR(100) NULL DEFAULT NULL AFTER `specialization`, ADD `post_office` VARCHAR(100) NULL DEFAULT NULL AFTER `village`, ADD `police_station` VARCHAR(100) NULL DEFAULT NULL AFTER `post_office`, ADD `district` VARCHAR(100) NULL DEFAULT NULL AFTER `police_station`, ADD `pincode` VARCHAR(100) NULL DEFAULT NULL AFTER `district`";
 	
 	$pdo->exec($sql1);
 	
