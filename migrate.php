13bd590f9f6e6862aa1431cee8f816cc4c2c22c0
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2026-05-19 02:48:22 +0530

    sfds

--- migrate.php
+++ migrate.php
@@ -5,7 +5,7 @@
 echo "Starting Migration Script...<br>";
 
 try {
-	$sql1 = "ALTER TABLE `settings_marksheet` ADD `hide_top_rankers` TINYINT(1) NOT NULL DEFAULT '0' AFTER `section_based_ranking`, ADD `hide_position_in_class` TINYINT(1) NOT NULL DEFAULT '0' AFTER `hide_top_rankers`";
+	$sql1 = "ALTER TABLE `settings_marksheet` ADD `marksheet_system` VARCHAR(20) NULL DEFAULT NULL AFTER `hide_position_in_class`;";
 	
 	$pdo->exec($sql1);
 	
