54db72a1d943b4579c3b7c49fc8042394298aa2d
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2026-06-02 18:42:51 +0530

    dfsd

--- migrate.php
+++ migrate.php
@@ -5,7 +5,29 @@
 echo "Starting Migration Script...<br>";
 
 try {
-	$sql1 = "ALTER TABLE `admin_auth_sessions` DROP `user_role`, DROP `username`, DROP `full_name`, DROP `security_pin`;";
+	$sql1 = "ALTER TABLE `admission_unpaid_fees` CHANGE `actual_amount` `actual_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `admission_unpaid_fees` CHANGE `unpaid_amount` `unpaid_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `admission_unpaid_fees` CHANGE `discount_amount` `discount_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `admission_partial_fees_payments` CHANGE `partial_paid_amount` `partial_paid_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `admission_full_paid_fees` CHANGE `actual_amount` `actual_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `admission_full_paid_fees` CHANGE `discount_amount` `discount_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `admission_full_paid_fees` CHANGE `total_paid_amount` `total_paid_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `admission_full_paid_fees` CHANGE `last_paid_amount` `last_paid_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `admission_fees_payment_history` CHANGE `payment_amount` `payment_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `admission_fees_payment_history` CHANGE `wallet_affected_balance` `wallet_affected_balance` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `student_full_paid_fees` CHANGE `actual_amount` `actual_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `student_full_paid_fees` CHANGE `discount_amount` `discount_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `student_full_paid_fees` CHANGE `total_paid_amount` `total_paid_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `student_full_paid_fees` CHANGE `last_paid_amount` `last_paid_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `student_partial_payments` CHANGE `partial_paid_amount` `partial_paid_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `student_payment_history` CHANGE `wallet_affected_balance` `wallet_affected_balance` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `student_payment_history` CHANGE `payment_amount` `payment_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `student_unpaid_fees` CHANGE `actual_amount` `actual_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `student_unpaid_fees` CHANGE `unpaid_amount` `unpaid_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `student_unpaid_fees` CHANGE `discount_amount` `discount_amount` DECIMAL(10,2) NULL DEFAULT NULL;
+ALTER TABLE `class_wise_monthly_fees` CHANGE `amount` `amount` DECIMAL(10,2) NOT NULL;
+ALTER TABLE `class_wise_additional_fees` CHANGE `amount` `amount` DECIMAL(10,2) NOT NULL;
+ALTER TABLE `class_wise_new_admission_fees` CHANGE `amount` `amount` DECIMAL(10,2) NOT NULL;";
 	
 	$pdo->exec($sql1);
 	
