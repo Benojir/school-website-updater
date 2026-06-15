acaea59097a0f994b66be515aedfb4a67a58b127
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2026-06-16 02:09:36 +0530

    xvcx

--- migrate.php
+++ migrate.php
@@ -6,7 +6,7 @@ echo "Starting Migration Script...<br>";
 
 try {
 	$sql1 = "ALTER TABLE `school_information` ADD `alternate_phone` VARCHAR(20) NULL DEFAULT NULL AFTER `phone`;
-ALTER TABLE `school_information` ADD `village` TEXT NULL DEFAULT NULL AFTER `address`, ADD `post_office` TEXT NULL DEFAULT NULL AFTER `village`, ADD `police_station` TEXT NULL DEFAULT NULL AFTER `post_office`, ADD `district` EXT NULL DEFAULT NULL AFTER `police_station`, ADD `pincode` VARCHAR(20) NULL DEFAULT NULL AFTER `district`;
+ALTER TABLE `school_information` ADD `village` TEXT NULL DEFAULT NULL AFTER `address`, ADD `post_office` TEXT NULL DEFAULT NULL AFTER `village`, ADD `police_station` TEXT NULL DEFAULT NULL AFTER `post_office`, ADD `district` TEXT NULL DEFAULT NULL AFTER `police_station`, ADD `pincode` VARCHAR(20) NULL DEFAULT NULL AFTER `district`;
 ALTER TABLE `school_information` ADD `google_map_embed_link` TEXT NULL DEFAULT NULL AFTER `google_map_link`;
 ALTER TABLE `school_information` ADD `campus_name` TEXT NULL DEFAULT NULL AFTER `pincode`;";
 	
