41d5369d9bdea5f3233918735c4a63f85fb2ded2
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2026-07-09 01:44:06 +0530

    cxv

--- migrate.php
+++ migrate.php
@@ -5,7 +5,7 @@
 echo "Starting Migration Script...<br>";
 
 try {
-	$sql = "ALTER TABLE `student_payment_history` CHANGE `id` `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT;";
+	$sql = "ALTER TABLE `admission_fees_payment_history` CHANGE `partial_payment_ids_backup` `partial_payment_ids_backup` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;";
 	
 	$pdo->exec($sql);
 	
