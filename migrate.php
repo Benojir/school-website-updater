4d0aa5208d1eab20f0b7782c73615ab23264b0ec
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2026-07-11 02:10:13 +0530

    sdfsd

--- migrate.php
+++ migrate.php
@@ -5,7 +5,13 @@
 echo "Starting Migration Script...<br>";
 
 try {
-	$sql = "ALTER TABLE `website_config` ADD `auto_request_monthly_fees_day` VARCHAR(5) NULL DEFAULT NULL AFTER `timezone`;";
+	$sql = "ALTER TABLE `sections` ADD `chat_id` TEXT NULL DEFAULT NULL AFTER `section_name`, ADD `bot_token` TEXT NULL DEFAULT NULL AFTER `chat_id`;
+ALTER TABLE `school_information` ADD `feedback_tg_chat_id` TEXT NULL DEFAULT NULL AFTER `whatsapp`;
+ALTER TABLE `website_config` ADD `telegram_bot_token` TEXT NULL DEFAULT NULL AFTER `imgbb_api_key`;
+ALTER TABLE `website_config` CHANGE `razorpay_charge_percentage` `razorpay_charge_percentage` DECIMAL(10,5) NOT NULL DEFAULT '0';
+ALTER TABLE `website_config` CHANGE `gst_on_razorpay_charge` `gst_on_razorpay_charge` DECIMAL(10,5) NOT NULL DEFAULT '0';";
+
+	echo "<span class='text-info'>Executing $sql</span>";
 	
 	$pdo->exec($sql);
 	
