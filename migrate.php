ab94cfeb3317c21048b1aa4d19fd58e0955a788b
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2026-08-01 00:30:00 +0530

    dsf

--- migrate.php
+++ migrate.php
@@ -5,16 +5,7 @@
 echo "Starting Migration Script...<br>";
 
 try {
-	$sql = "ALTER TABLE students 
-ADD COLUMN banglar_shiksha_code VARCHAR(255) NULL UNIQUE AFTER mother_occupation,
-ADD COLUMN caste VARCHAR(50) NULL AFTER banglar_shiksha_code,
-ADD COLUMN father_annual_income DECIMAL(10,2) NULL AFTER caste,
-ADD COLUMN mother_annual_income DECIMAL(10,2) NULL AFTER father_annual_income,
-ADD COLUMN bank_name VARCHAR(150) NULL AFTER mother_annual_income,
-ADD COLUMN bank_account_no VARCHAR(100) NULL AFTER bank_name,
-ADD COLUMN ifsc_code VARCHAR(50) NULL AFTER bank_account_no;
-ALTER TABLE students
-ADD COLUMN bank_branch_name VARCHAR(255) DEFAULT NULL AFTER bank_name;";
+	$sql = "ALTER TABLE `school_information` ADD `foundation_name` VARCHAR(255) NULL DEFAULT NULL AFTER `name`;";
 
 	echo "<span class='text-info'>Executing $sql</span><br>";
 	
