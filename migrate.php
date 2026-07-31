0ec14f3474af72cc2bdb430c621dc53d5d426ce0
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2026-07-31 23:58:02 +0530

    fsd

--- migrate.php
+++ migrate.php
@@ -5,17 +5,16 @@
 echo "Starting Migration Script...<br>";
 
 try {
-	$sql = "ALTER TABLE teacher_auth_sessions ADD CONSTRAINT fk_teachers_auth FOREIGN KEY (teacher_id) REFERENCES teachers(id) ON DELETE CASCADE;
-ALTER TABLE `teacher_auth_sessions` ADD `fcm_token` TEXT NULL DEFAULT NULL AFTER `device_name`;
-ALTER TABLE `teacher_auth_sessions` ADD `app_version` VARCHAR(20) NULL DEFAULT NULL AFTER `ip_address`;
-ALTER TABLE `teacher_auth_sessions` ADD `created_at` TIMESTAMP NOT NULL AFTER `app_version`;
-ALTER TABLE `teacher_auth_sessions` CHANGE `updated_at` `last_activity` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE `teacher_auth_sessions` DROP FOREIGN KEY `fk_teacher_accounts`;
-DROP TABLE IF EXISTS `attendance`;
-ALTER TABLE `mobile_notification_logs` CHANGE `id` `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT;
-RENAME TABLE mobile_notification_logs TO student_notifications_log;
-ALTER TABLE `website_config` ADD `file_storage_channel_id` TEXT NULL DEFAULT NULL AFTER `telegram_bot_token`;
-ALTER TABLE `student_homeworks` ADD `posted_by` VARCHAR(100) NULL DEFAULT NULL AFTER `status`;";
+	$sql = "ALTER TABLE students 
+ADD COLUMN banglar_shiksha_code VARCHAR(255) NULL UNIQUE AFTER mother_occupation,
+ADD COLUMN caste VARCHAR(50) NULL AFTER banglar_shiksha_code,
+ADD COLUMN father_annual_income DECIMAL(10,2) NULL AFTER caste,
+ADD COLUMN mother_annual_income DECIMAL(10,2) NULL AFTER father_annual_income,
+ADD COLUMN bank_name VARCHAR(150) NULL AFTER mother_annual_income,
+ADD COLUMN bank_account_no VARCHAR(100) NULL AFTER bank_name,
+ADD COLUMN ifsc_code VARCHAR(50) NULL AFTER bank_account_no;
+ALTER TABLE students
+ADD COLUMN bank_branch_name VARCHAR(255) DEFAULT NULL AFTER bank_name;";
 
 	echo "<span class='text-info'>Executing $sql</span><br>";
 	
