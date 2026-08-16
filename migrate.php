04251124bb2b7669015665001092dc8f3ac3a97b
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2026-08-17 00:56:19 +0530

    gfh

--- migrate.php
+++ migrate.php
@@ -12,10 +12,6 @@ try {
 	$pdo->exec($sql);
 	
 	echo "Database migration successfully done! 😅<br>";
-	// Delete the .sql file now
-	if (unlink($file_path)) {
-		echo "SQL file deleted successfully.<br>";
-	}
 
 } catch (PDOException $e) {
     echo "Database migration failed! Error: " . $e->getMessage() . "<br>";
